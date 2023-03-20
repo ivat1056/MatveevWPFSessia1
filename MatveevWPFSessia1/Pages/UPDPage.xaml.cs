8ab217d38b0a69bf80235e91a4eae69ac7bc8f01
иван матвеев <ivat1056@gmail.com>
2023-03-20 14:55:56 +0300

    20/03/23

--- MatveevWPFSessia1/Pages/UPDPage.xaml.cs
+++ MatveevWPFSessia1/Pages/UPDPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MatveevWPFSessia1.Pages
 {
@@ -32,7 +20,10 @@ namespace MatveevWPFSessia1.Pages
 
         private void btnBasket_Click(object sender, RoutedEventArgs e)
         {
-
+            //order.OrderStatusID = cbStatus.SelectedIndex + 1;
+            //Base.ep.SaveChanges();
+            //order.OrderStatusID = cbStatus.SelectedIndex + 1;
+            //Base.ep.SaveChanges();
 
         }
     }
